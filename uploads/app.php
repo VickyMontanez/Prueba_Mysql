90a06f6a927a06038013f3ceee1db1fa16b9db04
VickyMontanezCampus <vmontanez707@gmail.com>
2023-06-28 09:19:00 -0500

    feat: :truck: Config and connection of routes
    Config and connections of database with the new class

--- uploads/app.php
+++ uploads/app.php
@@ -1,8 +1,21 @@
 <?php
     require_once "../vendor/autoload.php";
+    trait getInstance{
+        public static $instance;
+        public static function getInstance(){
+            $arg = func_get_args();
+            $arg = array_pop($arg);
+            return (!(self::$instance instanceof self) || !empty($arg)) ? self::$instance = new static(...(array) $arg) : self::$instance;
+        }
+        function __set($name, $value){
+            $this->$name = $value;
+        }
+    }
+
     $router = new \Bramus\Router\Router();
     $router->get("/", function(){
-        echo "HP POR FIN";
+        echo "SIRVE?";
+        campers::getInstance();
     });
     $router->run();
 ?>
